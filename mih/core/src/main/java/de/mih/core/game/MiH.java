2ba5245d0d6f76141ed4f8245f7be5cdecc29a79
Cataract92 <salatgurke92@hotmail.de>
2016-04-23 12:05:35 +0200

    Set Game to higher resolution

--- mih/core/src/main/java/de/mih/core/game/MiH.java
+++ mih/core/src/main/java/de/mih/core/game/MiH.java
@@ -1,6 +1,7 @@
 package de.mih.core.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import de.mih.core.engine.gamestates.GameStateManager;
 import de.mih.core.game.gamestates.IntroGameState;
 import de.mih.core.game.gamestates.MainMenuGameState;
@@ -23,6 +24,8 @@ public class MiH extends ApplicationAdapter
 
 	public void create()
 	{
+
+		Gdx.graphics.setDisplayMode(1600,900,false);
 
 		this.gamestateManager = new GameStateManager();
 		IntroGameState    intro    = new IntroGameState(gamestateManager);

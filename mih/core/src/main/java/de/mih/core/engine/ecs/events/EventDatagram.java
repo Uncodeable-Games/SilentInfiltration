af602ad463cf841bdd5c52e842c86783989c92f3
Tobias Arens <arenst@hochschule-trier.de>
2016-05-18 22:14:49 +0200

    Added eventdatagram for networking

---
+++ mih/core/src/main/java/de/mih/core/engine/ecs/events/EventDatagram.java
@@ -0,0 +1,8 @@
+package de.mih.core.engine.ecs.events;
+
+import de.mih.core.engine.network.server.datagrams.BaseDatagram;
+
+public class EventDatagram extends BaseDatagram
+{
+	public BaseEvent event;
+}
